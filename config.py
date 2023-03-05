b220140be3050ff0b43e814825b84b3aa705b26b
Bernward Sanchez <sanzbernward@gmail.com>
2023-03-05 17:57:19 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# OpenAI API Key
+OPENAI_API_KEY = "your_api_key"
+
+# OpenAI API Endpoint
+OPENAI_API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
+
+# OpenAI GPT-3 Model
+OPENAI_MODEL = "gpt-3.5-turbo"
